a934ae04d9636f1aa4e62ababa825807e27693c6
nadiasupti <150934119+nadiasupti@users.noreply.github.com>
2024-06-22 15:37:29 +0600

    Update job.js

--- backend/models/job.js
+++ backend/models/job.js
@@ -1,14 +1,20 @@
-import mongoose from 'mongoose';
-
+// models/job.js
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const jobSchema = new Schema({
-  title: String,
-  description: String,
-  
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  company: { type: String, required: true },
+  companyLogo: { type: String, required: true },
+  location: { type: String, required: true },
+  type: { type: String, required: true },
+  salary: { type: String, required: true },
+  date: { type: Date, required: true }
+}, {
+  timestamps: true,
 });
 
 const Job = mongoose.model('Job', jobSchema);
 
-export default Job; 
-
+module.exports = Job;
