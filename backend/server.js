028e50ebf7c4f5aa737e545bde5ef1397cd2b465
Mahia Rahman <140752725+mahiOSD@users.noreply.github.com>
2024-07-04 21:56:27 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 import express from 'express';
+import path from 'path';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import jobsRouter from './routes/jobs.js'; 
-import authRouter from './routes/auth.js'; 
+import jobsRouter from './routes/jobs.js';
+import authRouter from './routes/auth.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+const __dirname = path.resolve();
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true 
+  origin: 'http://localhost:5173',
+  credentials: true
 }));
 app.use(express.json());
+
+// Serve static files
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -29,10 +33,8 @@ mongoose.connect(process.env.MONGODB_URI, {
   console.error('Could not connect to MongoDB:', err);
 });
 
-
-app.use('/api/auth', authRouter); 
-app.use('/api/jobs', jobsRouter); 
-
+app.use('/api/auth', authRouter);
+app.use('/api/jobs', jobsRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
