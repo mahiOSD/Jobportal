1ca904d1149c048011f6a7a07a00f1021ae0a3db
nadiasupti <150934119+nadiasupti@users.noreply.github.com>
2024-07-06 12:13:31 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
+
 app.use(express.json());
 
 mongoose
