92aa41412e032ab7bcf6e829495cc70e2a3275e1
nadiasupti <150934119+nadiasupti@users.noreply.github.com>
2024-07-06 12:21:10 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 
