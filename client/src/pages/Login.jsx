0310da40432ee11836ea0da765c99434ddee510b
nadiasupti <150934119+nadiasupti@users.noreply.github.com>
2024-07-17 02:16:54 +0600

    Update1 Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,6 @@
+//login.jsx
+
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -15,10 +18,11 @@ const Login = ({ setUser }) => {
     setError(''); // Clear previous errors
 
     try {
-      const response = await axios.post(https://jobportal-black.vercel.app/api/auth/login, {
-        email,
-        password,
-      });
+      const response = await axios.post(
+        'https://jobportal-black.vercel.app/api/auth/login',
+        { email, password },
+        { withCredentials: true } // Include credentials
+      );
 
       if (response.data && response.data.token) {
         localStorage.setItem('token', response.data.token);
