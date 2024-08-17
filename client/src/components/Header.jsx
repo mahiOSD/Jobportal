06dc5cf7d1cf635808fe7b20a8563045e7983893
Mahia Rahman <140752725+mahiOSD@users.noreply.github.com>
2024-08-18 02:10:51 +0600

    Change Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faBriefcase, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { faBriefcase, faPlusSquare, faSearch, faBars, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 const Header = ({ user, setUser }) => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [dashboardVisible, setDashboardVisible] = useState(false);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -19,65 +20,63 @@ const Header = ({ user, setUser }) => {
     setDropdownVisible(!dropdownVisible);
   };
 
+  const toggleDashboard = () => {
+    setDashboardVisible(!dashboardVisible);
+  };
+
   return (
     <header className="header">
-      <div className="logo-container">
-        <a href="/" className="icon-link">
-          <img src="/images/JobZeelogo.png" alt="logo" className="small-logo" />
-        </a>
-        <div className="logo">JobPortal</div>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          {user ? (
-            <>
-              <li>
-                <Link to="/search" className="nav-link">
-                  <FontAwesomeIcon icon={faSearch} className="nav-icon" />
-                  Start a search
-                </Link>
-              </li>
-              <li>
-                <Link to="/jobs" className="nav-link">
-                  <FontAwesomeIcon icon={faBriefcase} className="nav-icon" />
-                  My Jobs
-                </Link>
-              </li>
-              <li>
-                <Link to="/add-job" className="nav-link">
-                  <FontAwesomeIcon icon={faPlusSquare} className="nav-icon" />
-                  Post A Job
-                </Link>
-              </li>
-              <li className="profile-icon" onClick={toggleDropdown}>
-                <img
-                  src={user?.profilePicture || '/default-avatar.png'}
-                  alt="Profile Icon"
-                  className="profile-picture-icon"
-                />
-                {dropdownVisible && (
-                  <div className="dropdown-menu">
-                    <Link to="/profile" className="dropdown-item">My Profile</Link>
-                    <button className="dropdown-item" onClick={handleLogout}>Logout</button>
-                  </div>
-                )}
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to="/login" className="nav-link">Login</Link>
-              </li>
-              <li>
-                <Link to="/signup" className="nav-link">Sign Up</Link>
-              </li>
-            </>
+      <div className="header-content">
+        {/* Dashboard Icon */}
+        <div className="dashboard-icon" onClick={toggleDashboard}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+
+        {/* Logo and JobPortal Text */}
+        <div className="logo-container">
+          <a href="/search">
+            <img src="/images/JobZeelogo.png" alt="JobPortal Logo" className="small-logo" />
+          </a>
+          <div className="logo">JobPortal</div>
+        </div>
+
+        {/* Profile Icon */}
+        <div className="profile-container">
+          <div className="profile-icon" onClick={toggleDropdown}>
+            <img
+              src={user?.profilePicture || '/default-avatar.png'}
+              alt="Profile Icon"
+              className="profile-picture-icon"
+            />
+          </div>
+          {dropdownVisible && (
+            <div className="dropdown-menu">
+              <Link to="/profile" className="dropdown-item">My Profile</Link>
+            </div>
           )}
-        </ul>
-      </nav>
+        </div>
+      </div>
+
+      {/* Dashboard Menu */}
+      <div className={`dashboard-menu ${dashboardVisible ? 'show' : ''}`}>
+        <Link to="/search" className="dashboard-item">
+          <FontAwesomeIcon icon={faSearch} className="nav-icon" />
+          Start a Search
+        </Link>
+        <Link to="/jobs" className="dashboard-item">
+          <FontAwesomeIcon icon={faBriefcase} className="nav-icon" />
+          My Jobs
+        </Link>
+        <Link to="/add-job" className="dashboard-item">
+          <FontAwesomeIcon icon={faPlusSquare} className="nav-icon" />
+          Post A Job
+        </Link>
+        <div className="spacer"></div>
+        <button onClick={handleLogout} className="dashboard-item logout">
+          <FontAwesomeIcon icon={faSignOutAlt} className="nav-icon" />
+          Logout
+        </button>
+      </div>
     </header>
   );
 };
