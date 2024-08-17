2f5b5edab4595af68e96b929d665ea0c01c8dce0
Mahia Rahman <140752725+mahiOSD@users.noreply.github.com>
2024-08-18 02:14:51 +0600

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaBriefcase, FaSearch, FaUser } from 'react-icons/fa';
+import PropTypes from 'prop-types';  
 import './Home.css';
 
-const Home = () => {
+const Home = ({ user }) => {
   return (
     <div className="home">
-      <video autoPlay loop muted playsInline className="background-video">
-        <source src="/background-video.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-
+      {!user && (
+        <video autoPlay loop muted playsInline className="background-video">
+          <source src="/background-video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      )}
       <div className="home-banner">
         <h1>The Easiest Way to Get Your New Job</h1>
         <p>Your one-stop destination for finding your dream job</p>
@@ -31,10 +33,14 @@ const Home = () => {
             <p>Showcase your skills and experience</p>
           </div>
         </div>
-        <Link to="/signup" className="cta-button">Get Started</Link>
+        {!user && <Link to="/signup" className="cta-button">Get Started</Link>}
       </div>
     </div>
   );
+};
+
+Home.propTypes = {
+  user: PropTypes.object,  
 };
 
 export default Home;
