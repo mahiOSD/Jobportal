d861cde727381c8fa14e9ac4b9b169a0d1ef974e
Mahia Rahman <140752725+mahiOSD@users.noreply.github.com>
2024-08-18 15:05:09 +0600

    Change Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,62 +28,75 @@ const Header = ({ user, setUser }) => {
     <header className="header">
       <div className="header-content">
         {/* Dashboard Icon */}
-        <div className="dashboard-icon" onClick={toggleDashboard}>
-          <FontAwesomeIcon icon={faBars} />
-        </div>
+        {user && (
+          <div className="dashboard-icon" onClick={toggleDashboard}>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+        )}
 
         {/* Logo and JobPortal Text */}
         <div className="logo-container">
-          <a href="/search">
+          <Link to="/search">
             <img src="/images/JobZeelogo.png" alt="JobPortal Logo" className="small-logo" />
-          </a>
+          </Link>
           <div className="logo">JobPortal</div>
         </div>
 
-        {/* Profile Icon */}
-        <div className="profile-container">
-          <div className="profile-icon" onClick={toggleDropdown}>
-            <img
-              src={user?.profilePicture || '/default-avatar.png'}
-              alt="Profile Icon"
-              className="profile-picture-icon"
-            />
-          </div>
-          {dropdownVisible && (
-            <div className="dropdown-menu">
-              <Link to="/profile" className="dropdown-item">My Profile</Link>
+        {/* Profile or Auth Links */}
+        <div className="auth-container">
+          {user ? (
+            <div className="profile-container">
+              <div className="profile-icon" onClick={toggleDropdown}>
+                <img
+                  src={user?.profilePicture || '/default-avatar.png'}
+                  alt="Profile Icon"
+                  className="profile-picture-icon"
+                />
+              </div>
+              {dropdownVisible && (
+                <div className="dropdown-menu">
+                  <Link to="/profile" className="dropdown-item">My Profile</Link>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="auth-links">
+              <Link to="/login" className="auth-link">Log in</Link>
+              <Link to="/signup" className="auth-link">Sign Up</Link>
             </div>
           )}
         </div>
       </div>
 
       {/* Dashboard Menu */}
-      <div className={`dashboard-menu ${dashboardVisible ? 'show' : ''}`}>
-        <Link to="/search" className="dashboard-item">
-          <FontAwesomeIcon icon={faSearch} className="nav-icon" />
-          Start a Search
-        </Link>
-        <Link to="/jobs" className="dashboard-item">
-          <FontAwesomeIcon icon={faBriefcase} className="nav-icon" />
-          My Jobs
-        </Link>
-        <Link to="/add-job" className="dashboard-item">
-          <FontAwesomeIcon icon={faPlusSquare} className="nav-icon" />
-          Post A Job
-        </Link>
-        <div className="spacer"></div>
-        <button onClick={handleLogout} className="dashboard-item logout">
-          <FontAwesomeIcon icon={faSignOutAlt} className="nav-icon" />
-          Logout
-        </button>
-      </div>
+      {user && (
+        <div className={`dashboard-menu ${dashboardVisible ? 'show' : ''}`}>
+          <Link to="/search" className="dashboard-item">
+            <FontAwesomeIcon icon={faSearch} className="nav-icon" />
+            Start a Search
+          </Link>
+          <Link to="/jobs" className="dashboard-item">
+            <FontAwesomeIcon icon={faBriefcase} className="nav-icon" />
+            My Jobs
+          </Link>
+          <Link to="/add-job" className="dashboard-item">
+            <FontAwesomeIcon icon={faPlusSquare} className="nav-icon" />
+            Post A Job
+          </Link>
+          <div className="spacer"></div>
+          <button onClick={handleLogout} className="dashboard-item logout">
+            <FontAwesomeIcon icon={faSignOutAlt} className="nav-icon" />
+            Logout
+          </button>
+        </div>
+      )}
     </header>
   );
 };
 
 Header.propTypes = {
   user: PropTypes.object,
-  setUser: PropTypes.func,
+  setUser: PropTypes.func.isRequired,
 };
 
 export default Header;
