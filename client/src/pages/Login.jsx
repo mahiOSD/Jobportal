779dc59f7516fb9e9a041c90a6c4287c5172b032
Mahia Rahman <140752725+mahiOSD@users.noreply.github.com>
2024-09-18 22:28:22 +0600

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,23 +14,29 @@ const Login = ({ setUser }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(''); 
-  
+
     try {
       const response = await axios.post('https://jobportal-black.vercel.app/api/auth/login', {
         email,
         password,
         category, 
       });
-  
+
       if (response.data && response.data.token && response.data.refreshToken) {
         localStorage.setItem('token', response.data.token); 
         localStorage.setItem('refreshToken', response.data.refreshToken); 
         localStorage.setItem('user', JSON.stringify(response.data.user));
-        localStorage.setItem('userCategory', category); 
+        localStorage.setItem('userCategory', response.data.user.category); 
         
         setUser(response.data.user);  
-        navigate('/dashboard');
-  
+
+       
+        if (response.data.user.category === 'admin') {
+          navigate('/dashboard');
+        } else {
+          navigate('/search');
+        }
+        
         window.dispatchEvent(new Event('userLoggedIn')); 
       } else {
         setError('Login failed: Invalid response');
@@ -39,7 +45,6 @@ const Login = ({ setUser }) => {
       setError(error.response?.data?.message || 'An error occurred. Please try again.');
     }
   };
-  
 
   return (
     <div className="login-container">
