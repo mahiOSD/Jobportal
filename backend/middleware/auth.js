c47976b2016f92ec5f042fb4983c6d1b1245cb1e
Mahia Rahman <140752725+mahiOSD@users.noreply.github.com>
2024-09-18 22:01:43 +0600

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -15,20 +15,22 @@ const auth = (allowedRoles) => (req, res, next) => {
     return res.status(403).json({ message: 'No token provided' });
   }
   try {
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.user = decoded;  
     console.log('Decoded User:', req.user);
-    if (!allowedRoles.includes(req.user.category)) {
+    
+    
+    if (allowedRoles && !allowedRoles.includes(req.user.category)) {
       console.log('Insufficient permissions');
       return res.status(403).json({ message: 'Insufficient permissions' });
     }
+
     next();
   } catch (error) {
     console.log('Invalid token:', error);
     return res.status(403).json({ message: 'Invalid token' });
   }
 };
-
-
 
 export default auth;
