fadd9ccc7511e7aa76970f5a7e4b1b5c4a87e8c0
nadiasupti <150934119+nadiasupti@users.noreply.github.com>
2024-09-22 00:14:00 +0600

    Update profile.js

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1024 * 1024 * 5 }, 
+  limits: { fileSize: 1024 * 1025 * 5 }, 
 });
 
 
